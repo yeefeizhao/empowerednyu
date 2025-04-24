995ef66f69007a79a1ff0209bfc9ea4a0554e280
yifei <yifeizhaogege@gmail.com>
2025-04-24 16:40:31 -0400

    services

--- src/ServicesOption.js
+++ src/ServicesOption.js
@@ -1,12 +1,17 @@
 import React from "react";
-import './ServicesOption.css'
+import "./ServicesOption.css";
 
-function ServicesOption({ title, description }) {
+function ServicesOption({ number, title, description }) {
     return (
         <div className="services-option">
             <div className="services-option-container">
-                <div className="services-option-circle"><h2>{title}</h2></div>
-                <div className="services-option-description">{description}</div>
+                <div className="services-option-number">
+                    <h2>{number}.</h2>
+                </div>
+                <div className="services-option-content">
+                    <h2>{title}</h2>
+                    <p>{description}</p>
+                </div>
             </div>
         </div>
     );
