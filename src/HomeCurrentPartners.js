f502aed1669cc6e3ffb5f1ad67c378db786c10db
yifei <yifeizhaogege@gmail.com>
2025-09-14 22:38:20 -0400

    current partners

--- src/HomeCurrentPartners.js
+++ src/HomeCurrentPartners.js
@@ -1,6 +1,5 @@
 import React from "react";
 import "./HomeCurrentPartners.css";
-import brac from "./images/current/brac.png";
 import etm from "./images/current/etm.jpg";
 import farm from "./images/current/farm-school.jpg";
 import luni from "./images/current/luniemere.png";
@@ -13,14 +12,9 @@ function HomeCurrentPartners() {
                     <h2>Current Partners</h2>
                 </div>
                 <div className="current-partners-logos">
-                    <div className="current-partners-row">
-                        <img src={brac} alt="BRAC" />
-                        <img src={etm} alt="ETM" />
-                    </div>
-                    <div className="current-partners-row">
-                        <img src={farm} alt="Farm School" />
-                        <img src={luni} alt="Luniemere" />
-                    </div>
+                    <img src={etm} alt="ETM" />
+                    <img src={farm} alt="Farm School" />
+                    <img src={luni} alt="Luniemere" />
                 </div>
                 <div className="current-partners-end"></div>
             </div>
