88c4be328cc3f1c9523cde27bccc96cead3322cc
yifei <yifeizhaogege@gmail.com>
2025-09-29 10:58:25 -0400

    rotate api

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyArVW3mSVlCxUCOMEUHlZv2GN4Vp70nwtU",
-    authDomain: "empowered-nyu.firebaseapp.com",
-    projectId: "empowered-nyu",
-    storageBucket: "empowered-nyu.firebasestorage.app",
-    messagingSenderId: "819490691955",
-    appId: "1:819490691955:web:636d5dd5fc272c0190c306",
-    measurementId: "G-4C8ZHEQHM7",
+    apiKey: process.env.API_KEY,
+    authDomain: process.env.AUTH_DOMAIN,
+    projectId: process.env.PROJECT_ID,
+    storageBucket: process.env.STORAGE_BUCKET,
+    messagingSenderId: process.env.MESSAGING_SENDER_ID,
+    appId: process.env.APP_ID,
+    measurementId: process.env.MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
